1fdc131556e82dd8e9e426baa51a4f66a8b39d6c
Foysal Ahmed <foysal.ahmed6862@gmail.com>
2022-10-03 16:21:17 +0600

    for loop, while loop, do while loop

---
+++ Loop/do_while_loop.php
@@ -0,0 +1,14 @@
+<?php
+// do while loop execute statement then check condition
+
+// $i = 1;
+// while($i<1){
+//     echo $i;
+//     $i++;
+// }
+
+$i =1;
+do{
+    echo $i;
+    $i++;
+}while($i<1);
